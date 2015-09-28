252319fd1b0202dfb725e163934708b9fcb3cbc5
Luis Perez <luisperez@college.harvard.edu>
2015-09-28 13:23:11 -0400

    Starting implementation of the hash_map

--- src/hash_map.c
+++ src/hash_map.c
@@ -1,2 +1,22 @@
 #include "hash_map.h"
 
+// The has function used by this particular implementation.
+// Source (http://stackoverflow.com/questions/7666509/hash-function-for-string)
+unsigned long hash(unsigned char* str) {
+    unsigned long hash = 5381;
+    int c;
+
+    while (c = *str++)
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+    return hash;
+}
+
+//
+bool add_element(cont_node* node, const) {
+
+}
+
+bool insert(hash_map* map, const char* key, const void* value) {
+
+}
